40585b4ca685e2913da4a4d207e5cd68f9c9a196
JiEung2 <181757@jnu.ac.kr>
2023-03-12 16:58:33 +0900

    뒤집은 소수

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -8,23 +8,32 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		System.out.println(T.solution(n));
-	}
-	
-	public int solution(int n) {
-		int answer=0;
-		int[] ch = new int[n+1];
+		int[] arr = new int[n];
 		
-		for(int i=2; i<=n; i++) {
-			if(ch[i]==0) {
-				answer++;
-				for(int j=i; j<=n; j+=i) {
-					ch[j]=1;
-				}
-			}
-			
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		}
+		for(int x : T.solution(arr, n)) {
+			System.out.print(x+" ");
+		}
+		
+	}
+	public ArrayList<Integer> solution(int[] arr, int n) {
+		ArrayList<Integer> answer = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			if(check(arr[i])) answer.add(arr[i]);
 		}
 		
 		return answer;
+	}
+	public boolean check(int n) {
+		if(n==1) return false;
+		else {
+			for(int i=2; i<=n/2; i++) {
+				if(n%i==0) return false;
+			}
+		}
+		return true;
 	}
 }
