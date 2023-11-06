23b219826089b6061e1c8f8a2fa43169105b2fa1
JiEung2 <181757@jnu.ac.kr>
2023-11-06 14:20:09 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        int[] arr = new int[progresses.length];
+        
+        for(int i=0; i<progresses.length; i++){
+            int r = 100 - progresses[i];
+            if(r % speeds[i] != 0) arr[i] = r / speeds[i] + 1;
+            else arr[i] = r / speeds[i];
+        }
+        
+        int tmp = arr[0];
+        int cnt = 1;
+        for(int i=1; i<arr.length; i++){
+            if(tmp < arr[i]){
+                tmp = arr[i];
+                answer.add(cnt);
+                cnt = 1;
+            }
+            else {
+                cnt++;  
+            }
+        }
+        answer.add(cnt);
+        
+        return answer;
+    }
+}
