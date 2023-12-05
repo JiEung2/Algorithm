1191660432f223c006bc9ef5d92c7cae9af8f800
JiEung2 <181757@jnu.ac.kr>
2023-12-05 16:40:43 +0900

    [level 2] Title: 피보나치 수, Time: 2.30 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int[] Fibo = new int [n+1];
+        Fibo[0] = 0;
+        Fibo[1] = 1;
+        for(int i=2; i<=n; i++){
+            Fibo[i] = (Fibo[i-2] + Fibo[i-1]) % 1234567;
+        }
+        return Fibo[n];
+    }
+}
