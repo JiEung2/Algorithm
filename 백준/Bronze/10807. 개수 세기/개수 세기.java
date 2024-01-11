2176dcbb9a04db45400549f0bd65e6579e5c8dab
JiEung2 <181757@jnu.ac.kr>
2024-01-11 09:06:04 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for(int i=0; i<N; i++){
+            if(arr[i] == v) cnt++;
+        }
+        System.out.println(cnt);
+	}
+}
