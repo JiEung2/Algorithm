0256c068924d0b731be3582de7929b7ddfba5f5e
JiEung2 <181757@jnu.ac.kr>
2024-01-15 16:33:16 +0900

    [level Lv.2] Title: 귤 고르기, Time: 65.24 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<tangerine.length; i++){
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0) + 1);
+        }
+        
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+        keySet.sort((o1,o2) -> map.get(o2).compareTo(map.get(o1)));
+        
+        int cnt = 0;
+        for(Integer key : keySet){
+            cnt++;
+            k -= map.get(key);
+            if(k<=0) break;
+        }
+        answer = cnt;
+        return answer;
+    }
+}
