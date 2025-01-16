ff885d993ed626c5fef61e6faabedd9fe211a145
JiEung2 <181757@jnu.ac.kr>
2025-01-16 22:36:12 +0900

    [Bronze V] Title: 시험 성적, Time: 96 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        char result = ' ';
+        if (n >= 90) {
+            result = 'A';
+        } else if (n >= 80) {
+            result = 'B';
+        } else if (n >= 70) {
+            result = 'C';
+        } else if (n >= 60) {
+            result = 'D';
+        } else result = 'F';
+
+        System.out.println(result);
+    }
+}
