4a7e894ccb5784df3738b2429ef94209f8cc2cb3
JiEung2 <181757@jnu.ac.kr>
2025-10-21 22:10:50 +0900

    [level 1] Title: 폰켓몬, Time: 5.77 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,11 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = nums.length / 2;
-        int size = Arrays.stream(nums).distinct().toArray().length;
-        if(size < answer)
-            answer = size;
+        int answer = 0;
+        int n = nums.length / 2;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int number : nums) {
+            map.put(number, map.getOrDefault(number, 0) + 1);    
+        }
+        
+        int max = map.keySet().size();
+        if(n >= max) answer = max;
+        else answer = n;
+        
         return answer;
     }
-    
 }
