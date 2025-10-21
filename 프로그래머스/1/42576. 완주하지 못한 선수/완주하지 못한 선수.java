fc1bace3e0972fe089780c962b99e160bc834551
JiEung2 <181757@jnu.ac.kr>
2025-10-21 21:15:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.92 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,21 +3,21 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> map = new HashMap<>();
-        for(String p : participant){
-            map.put(p, map.getOrDefault(p, 0)+1);
+        
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
-        for(String c : completion){
-            map.put(c, map.get(c)-1);
+        
+        for(String c : completion) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
         }
-        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
-        while(iter.hasNext()){
-            Map.Entry<String, Integer> entry = iter.next();
-            if (entry.getValue() != 0){
-                answer = entry.getKey();
+        
+        for(String key : map.keySet()){
+            if (map.get(key) > 0) {
+                answer = key;
                 break;
             }
         }
-        
         return answer;
     }
 }
