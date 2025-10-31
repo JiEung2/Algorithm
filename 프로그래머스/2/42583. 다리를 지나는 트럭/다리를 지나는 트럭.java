11c8af26c43cc5b614592d64a35b07dc038bb827
JiEung2 <181757@jnu.ac.kr>
2025-10-31 21:20:19 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 11.75 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,36 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        int currentWeight = 0;
+    
+        Queue<int[]> q = new LinkedList<>();
+        int sum = truck_weights[0];
+        int count = 1;
+        int index = 1;
+        q.add(new int[] {count, truck_weights[0]});
         
-        for(int truck : truck_weights){
-            while(true){
-                if(queue.isEmpty()){
-                    queue.add(truck);
-                    currentWeight += truck;
-                    answer++;
-                    break;
+        while(!q.isEmpty()) {
+            count++;
+            int[] front = q.peek();
+            if(count - front[0] == bridge_length){
+                // System.out.println("now = " + count);
+                // System.out.println("out = " + front[1]);
+                q.poll();
+                sum -= front[1];
+                if(index < truck_weights.length && (sum + truck_weights[index]) <= weight) {
+                    sum += truck_weights[index];
+                    q.add(new int[] {count, truck_weights[index]});
+                    index++;
                 }
-                else if(queue.size() == bridge_length) {
-                    currentWeight -= queue.poll();
-                }
-                else{
-                    if(currentWeight + truck <= weight){
-                        queue.add(truck);
-                        currentWeight += truck;
-                        answer++;
-                        break;
-                    }
-                    else {
-                        queue.add(0);
-                        answer++;
-                    }
+            }
+            else{
+                if(index < truck_weights.length && (sum + truck_weights[index]) <= weight) {
+                    sum += truck_weights[index];
+                    q.add(new int[] {count, truck_weights[index]});
+                    index++;
                 }
             }
         }
-        return answer += bridge_length;
+        
+        return count;
     }
 }
