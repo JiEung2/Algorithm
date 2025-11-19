74bdab32c9268c89b4b836572593f591d8af2af7
JiEung2 <181757@jnu.ac.kr>
2025-11-19 20:42:48 +0900

    [level 1] Title: 모의고사, Time: 0.85 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,33 +2,23 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int[] answers) {
         List<Integer> answer = new ArrayList<>();
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] thr = {3,3,1,1,2,2,4,4,5,5};
-        int[] cnt = new int[3];
-        for(int i=0; i<answers.length; i++){
-            if(one[i%5] == answers[i]) cnt[0]++;
-            if(two[i%8] == answers[i]) cnt[1]++;
-            if(thr[i%10] == answers[i]) cnt[2]++;
+        int one[] = new int[] {1,2,3,4,5};
+        int two[] = new int[] {2,1,2,3,2,4,2,5};
+        int three[] = new int[] {3,3,1,1,2,2,4,4,5,5};
+        
+        int f = 0, s = 0, t = 0;
+        for(int i = 0 ; i < answers.length; i++) {
+            if(one[i % one.length] == answers[i]) f++;
+            if(two[i % two.length] == answers[i]) s++;
+            if(three[i % three.length] == answers[i]) t++;
         }
         
-        int max = 0;
-        int index = -1;
+        int max = Math.max(Math.max(f, s), t);
+        if(f == max) answer.add(1);
+        if(s == max) answer.add(2);
+        if(t == max) answer.add(3);
         
-        for(int i=0; i<3; i++){
-            if(max<cnt[i]){
-                max = cnt[i];
-                index = i+1;
-            }
-        }
-        answer.add(index);
-        for(int i=0; i<3; i++){
-            if(max==cnt[i] && i+1 != index){
-                answer.add(i+1);
-            }
-        }
-        
-        
+        Collections.sort(answer);
         
         return answer;
     }
