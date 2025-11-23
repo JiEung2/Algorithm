f3c11260bf1502d7214b9855901efcd3b0b9c6d0
JiEung2 <181757@jnu.ac.kr>
2025-11-23 19:16:47 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,16 +2,22 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int i=yellow; i>=1; i--){
-            if(yellow % i == 0){
-                int tmp = yellow / i;
-                if(((i + 2) * 2 + tmp * 2) == brown) {
-                    answer[0] = i+2;
-                    answer[1] = tmp+2;
+        int total = brown + yellow;
+        for(int i = 1; i < total; i++) {
+            if(total % i == 0) {
+                int num1 = i;
+                int num2 = total / i ;
+                int big = Math.max(num1, num2);
+                int small = Math.min(num1, num2);
+                
+                if((big - 2) * (small - 2) == yellow) {
+                    answer[0] = big;
+                    answer[1] = small;
                     break;
                 }
             }
         }
+        
         return answer;
     }
 }
