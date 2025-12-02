5671037f2ee84c19fe3db4528d21a49feaf14269
JiEung2 <181757@jnu.ac.kr>
2025-12-02 12:35:09 +0900

    [level 2] Title: 모음 사전, Time: 6.57 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,32 @@
 import java.util.*;
 class Solution {
     List<String> list = new ArrayList<>();
-
     public int solution(String word) {
         int answer = 0;
-        String a = "AEIOU";
-        list.add("");
-        for(int i=0; i<5; i++){
-            DFS(Character.toString(a.charAt(i)), a);
+        String[] arr = new String[] {"A", "E", "I", "O", "U"};
+        
+        for(int i = 0; i < arr.length; i++) {
+            dfs(arr[i], arr);
         }
-        for(int i=0; i<list.size(); i++){
-            if(list.get(i).equals(word)) answer = i;
+        
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).equals(word)) {
+                answer = i + 1;
+                break;
+            }
         }
+        
         return answer;
     }
     
-    public void DFS(String tmp, String word){
+    public void dfs(String tmp, String[] arr) {
         list.add(tmp);
         if(tmp.length() == 5) return;
-        for(int i=0; i<word.length(); i++){
-            tmp += word.charAt(i);
-            DFS(tmp, word);
-            tmp = tmp.substring(0, tmp.length()-1);
+        
+        for(int i = 0 ; i < arr.length; i++) {
+            tmp += arr[i];
+            dfs(tmp, arr);
+            tmp = tmp.substring(0, tmp.length() - 1);
         }
     }
 }
