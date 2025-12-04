cab1ea8c4d5e7fcb72805bd85de973d1c65a5a77
JiEung2 <181757@jnu.ac.kr>
2025-12-04 21:17:22 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,15 +1,19 @@
+import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int move = name.length()-1;
+        int move = name.length() - 1;
         
-        for(int i=0; i<name.length(); i++){
-            answer += Math.min((int)(name.charAt(i) - 'A'), (int)('Z' - name.charAt(i))+1);
-            int next = i+1;
-            while(next < name.length() && name.charAt(next) == 'A')
+        for(int i = 0; i < name.length(); i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            
+            int next = i + 1;
+            while (next < name.length() && name.charAt(next) == 'A') {
                 next++;
-            move = Math.min(move, i * 2 + (name.length() - next));
-            move = Math.min(move, i + (name.length() - next) * 2);
+            }
+            
+            move = Math.min(move, i * 2 + name.length() - next);
+            move = Math.min(move, (name.length() - next) * 2 + i);
         }
         
         return answer + move;
