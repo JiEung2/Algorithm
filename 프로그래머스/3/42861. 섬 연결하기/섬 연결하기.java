950867ca09b7969bedc96a887201ce6d110f5ed4
JiEung2 <181757@jnu.ac.kr>
2025-12-10 20:13:15 +0900

    [level 3] Title: 섬 연결하기, Time: 0.93 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -3,21 +3,17 @@ class Solution {
     int[] parent;
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        int line = 0;
         parent = new int[n];
-        
-        for(int i=0; i<parent.length; i++){
+        for(int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
-        Arrays.sort(costs, (o1,o2) -> Integer.compare(o1[2], o2[2]));
+        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
         
-        for(int i=0; i<costs.length; i++){
-            if(line == n - 1) break;
-            if(find(costs[i][0]) != find(costs[i][1])){
-                union(costs[i][0], costs[i][1]);
+        for(int i = 0; i < costs.length; i++) {
+            if(find(costs[i][0]) != find(costs[i][1])) {
                 answer += costs[i][2];
-                line++;
+                union(costs[i][0], costs[i][1]);
             }
         }
         
@@ -25,17 +21,18 @@ class Solution {
         return answer;
     }
     
-    public int find(int x){
+    public int find(int x) {
         if(parent[x] == x) return x;
         return find(parent[x]);
     }
     
-    public void union(int x, int y){
+    public void union(int x, int y) {
         x = find(x);
         y = find(y);
         
-        if(x != y){
+        if(x != y) {
             parent[y] = x;
         }
     }
+    
 }
