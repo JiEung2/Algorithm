48aa6a2d9c489548d828355bc0854040bccef7a3
JiEung2 <181757@jnu.ac.kr>
2025-12-10 21:26:38 +0900

    [level 3] Title: 단속카메라, Time: 7.23 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
-        Arrays.sort(routes, (o1,o2) -> Integer.compare(o1[1], o2[1]));
+        int answer = 1;
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[0], b[0]));
+        int now = routes[0][1];
         
-        int cam = Integer.MIN_VALUE;
-        
-        for(int[] route : routes){
-            if(cam < route[0]){
-                cam = route[1];
+        for(int i = 1; i < routes.length; i++) {
+            if(now  < routes[i][0]) {
+                now = routes[i][1];
                 answer++;
+                continue;
             }
+            now = Math.min(now, routes[i][1]);
         }
-        
         return answer;
     }
 }
