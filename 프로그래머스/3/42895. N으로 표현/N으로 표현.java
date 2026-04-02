8f968a586e2504d7728346b0a0292bce90c90e14
JiEung2 <181757@jnu.ac.kr>
2026-04-02 22:35:56 +0900

    [level 3] Title: N으로 표현, Time: 32.34 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,42 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        List<Set<Integer>> result = new ArrayList<>();
-        
+        List<Set<Integer>> list = new ArrayList<>();
         for(int i = 0; i < 9; i++) {
-            result.add(new HashSet<>());
+            list.add(new HashSet());
         }
         
-        result.get(1).add(N);
-        
+        list.get(1).add(N);
         for(int i = 2; i < 9; i++) {
-            Set<Integer> now = result.get(i);
-            
-            for(int j = 1; j <= i; j++) {
-                Set<Integer> pre = result.get(j);
-                Set<Integer> pos = result.get(i-j);
+            Set<Integer> now = list.get(i);
+            now.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            for(int j = 1; j < i; j++) {
+                Set<Integer> a = list.get(j);
+                Set<Integer> b = list.get(i-j);
                 
-                for(int num1 : pre) {
-                    for(int num2 : pos) {
+                for(int num1 : a) {
+                    for(int num2 : b) {
                         now.add(num1 + num2);
-                        now.add(num1 - num2);
                         now.add(num1 * num2);
-                        
-                        if(num1 != 0 && num2 != 0) {
-                            now.add(num1 / num2);
+                        now.add(num1 - num2);
+                        if(num1 != 0 && num2 != 0){
+                            now.add(num1 / num2); 
                         }
+                        
                     }
                 }
             }
-            now.add(Integer.parseInt(String.valueOf(N).repeat(i)));
         }
         
-        for(Set<Integer> set : result) {
-            if(set.contains(number)) {
-                return result.indexOf(set);
-            }
+        for(Set<Integer> set : list) {
+            if(set.contains(number)) return list.indexOf(set);
         }
         
-        return -1; 
+        return -1;
+        
     }
 }
