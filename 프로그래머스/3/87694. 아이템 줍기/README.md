dd4672eef381c9fa6e34f43aded2e4ef3012750b
JiEung2 <181757@jnu.ac.kr>
2026-04-03 22:12:01 +0900

    [level 3] Title: 아이템 줍기, Time: 1.29 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=java) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 1.46 ms
+메모리: 83.9 MB, 시간: 1.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 07일 21:18:05
+2026년 04월 03일 22:11:58
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -2,10 +2,11 @@ import java.util.*;
 class Solution {
     int[] dx = {0, 1, 0, -1};
     int[] dy = {1, 0, -1, 0};
+    int[][] check;
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        
         int max = 0;
+        
         for(int i = 0; i < rectangle.length; i++) {
             for(int j = 0; j < 4; j++) {
                 max = Math.max(max, rectangle[i][j]);
@@ -13,6 +14,13 @@ class Solution {
         }
         
         int[][] map = new int[max * 2 + 1][max * 2 + 1];
+        check = new int[max * 2 + 1][max * 2 + 1];
+        
+        for(int i = 0; i < check.length; i++) {
+            for(int j = 0; j < check.length; j++) {
+                check[i][j] = Integer.MAX_VALUE;
+            }
+        }
         
         for(int i = 0; i < rectangle.length; i++) {
             int x1 = rectangle[i][0] * 2;
@@ -32,54 +40,47 @@ class Solution {
             fillInside(map, x1, x2, y1, y2);
         }
         
-        int[][] check = new int[max * 2 + 1][max * 2 + 1];
-        
-        for(int i = 0; i < check.length; i++) {
-            for(int j = 0; j < check.length; j++) {
-                check[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        bfs(map, check, characterX * 2, characterY * 2);
+        bfs(map, characterX * 2, characterY * 2);
         
         return check[itemX * 2][itemY * 2] / 2;
     }
     
-    public void bfs(int[][] map, int[][] check, int x, int y) {
+    public void bfs(int[][] map, int cX, int cY) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x, y});
-        check[x][y] = 0;
+        q.add(new int[] {cX, cY});
+        check[cX][cY] = 0;
         
         while(!q.isEmpty()) {
             int[] point = q.poll();
-            int nx = point[0];
-            int ny = point[1];
+            int x = point[0];
+            int y = point[1];
             
             for(int i = 0; i < 4; i++) {
-                int newX = nx + dx[i];
-                int newY = ny + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(newX >= 0 && newX < map.length && newY >= 0 && newY < map[0].length && map[newX][newY] == 1) {
-                    if(check[nx][ny] + 1 < check[newX][newY]) {
-                        check[newX][newY] = check[nx][ny] + 1;
-                        q.add(new int[] {newX, newY});
+                if(nx >= 0 && nx < map.length && ny >= 0 && ny < map.length && map[nx][ny] == 1) {
+                    if(check[nx][ny] == Integer.MAX_VALUE) {
+                        check[nx][ny] = check[x][y] + 1;
+                        q.add(new int[] {nx, ny});
                     }
                 }
             }
         }
+        
     }
     
     public void fill(int[][] map, int x1, int x2, int y1, int y2) {
         for(int i = x1; i <= x2; i++) {
             for(int j = y1; j <= y2; j++) {
-                if(map[i][j] == 1) continue;
                 map[i][j] = 1;
             }
         }
     }
     
     public void fillInside(int[][] map, int x1, int x2, int y1, int y2) {
-        for(int i = x1 + 1; i < x2; i++) {
-            for(int j = y1 + 1; j < y2; j++) {
+        for(int i = x1+1; i < x2; i++) {
+            for(int j = y1+1; j < y2; j++) {
                 map[i][j] = 0;
             }
         }
