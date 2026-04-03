f4d41ca41396d82b5c63d54ed22249761698480f
JiEung2 <181757@jnu.ac.kr>
2026-04-03 22:37:27 +0900

    [level 3] Title: 여행경로, Time: 0.74 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -8,9 +8,9 @@ class Solution {
         visited = new boolean[tickets.length];
         
         Arrays.sort(tickets, (o1, o2) -> {
-            if(o1[0].equals(o2[0])) {
+            if(o1[0].equals(o2[0]))
                 return o1[1].compareTo(o2[1]);
-            }
+            
             return o1[0].compareTo(o2[0]);
         });
         
@@ -21,16 +21,15 @@ class Solution {
         return answer.toArray(new String[0]);
     }
     
-    public boolean dfs(String current, String[][] tickets, int count) {
-        if(count == tickets.length){
+    public boolean dfs(String dep, String[][] tickets, int count) {
+        if(count == tickets.length) {
             return true;
         }
         
         for(int i = 0; i < tickets.length; i++) {
-            if(!visited[i] && tickets[i][0].equals(current)) {
+            if(!visited[i] && dep.equals(tickets[i][0])) {
                 visited[i] = true;
                 answer.add(tickets[i][1]);
-                
                 if(dfs(tickets[i][1], tickets, count + 1)) {
                     return true;
                 }
@@ -41,4 +40,6 @@ class Solution {
         }
         return false;
     }
+    
+
 }
