69c6001e23bcf677ecefdcebd6c8dad087aa9216
JiEung2 <181757@jnu.ac.kr>
2026-04-03 21:14:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.25 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,47 +2,43 @@ import java.util.*;
 class Solution {
     int[] dx = {0, 1, 0, -1};
     int[] dy = {1, 0, -1, 0};
-    
+    int[][] check;
+    int n, m;
     public int solution(int[][] maps) {
-        int answer = 0;
-        int[][] check = new int[maps.length][maps[0].length];
-        for(int i = 0; i < check.length; i++) {
-            for(int j = 0; j < check[i].length; j++) {
-                check[i][j] = Integer.MAX_VALUE;
-            }
-        }
+        n = maps.length;
+        m = maps[0].length;
+        check = new int[n][m];
         
-        check[0][0] = 1;
-        bfs(maps, check, 0, 0);
-        
-        answer = check[maps.length - 1][maps[0].length - 1];
-        
-        if(answer == Integer.MAX_VALUE) answer = -1;
-        return answer;
+        bfs(maps, 0, 0);
+        if(check[maps.length - 1][maps[0].length - 1] == 0) return -1;
+        return check[maps.length - 1][maps[0].length - 1];
     }
     
-    public void bfs(int[][] maps, int[][] check, int x, int y) {
+    public void bfs(int[][] maps, int a, int b) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x, y});
+        q.add(new int[]{a, b});
+
+
         
-        while (!q.isEmpty()) {
+        check[a][b] = 1;
+        
+        while(!q.isEmpty()) {
             int[] point = q.poll();
-            int nx = point[0];
-            int ny = point[1];
+            int x = point[0];
+            int y = point[1];
             
             for(int i = 0; i < 4; i++) {
-                int newX = dx[i] + nx;
-                int newY = dy[i] + ny;
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(newX >= 0 && newX < maps.length && newY >= 0 && newY < maps[0].length && maps[newX][newY] == 1) {
-                    if(check[nx][ny] + 1 < check[newX][newY]) {
-                        q.add(new int[] {newX, newY});
-                        check[newX][newY] = check[nx][ny] + 1;
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
+                    if(check[nx][ny] == 0) {
+                        check[nx][ny] = check[x][y] + 1;
+                        q.add(new int[] {nx, ny});
                     }
                 }
             }
+            
         }
-        
     }
-    
 }
