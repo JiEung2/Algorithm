60c26fb98485523da6974a57274065bcc2f10358
JiEung2 <181757@jnu.ac.kr>
2026-04-04 16:21:18 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 12.56 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,143 +1,135 @@
 import java.util.*;
 class Solution {
-    List<List<Point>> t = new ArrayList<>();
-    List<List<Point>> g = new ArrayList<>();
-    int[] dx = {0, 0, 1, -1};
-    int[] dy = {1, -1, 0, 0};
+    int[] dx = new int[] {0, 1, 0, -1};
+    int[] dy = new int[] {1, 0, -1, 0};
+    List<List<int[]>> g;
+    List<List<int[]>> t;
     
     public int solution(int[][] game_board, int[][] table) {
-        int answer = 0;
-        int len = game_board.length;
+        int result = 0;
         
-        for(int i=0; i<len; i++){
-            for(int j=0; j<len; j++){
-                if(game_board[i][j]==1){
-                    game_board[i][j] = 0;
+        g = new ArrayList<>();
+        t = new ArrayList<>();
+        
+        for(int i = 0; i < game_board.length; i++) {
+            for(int j = 0; j < game_board.length; j++) {
+                if(game_board[i][j] == 0) g.add(bfs(0, game_board, i, j));
+                if(table[i][j] == 1) t.add(bfs(1, table, i, j));
+            }
+        }
+        
+        boolean[] check = new boolean[t.size()];
+        
+        for(int i = 0; i < g.size(); i++) {
+            for(int j = 0; j < t.size(); j++) {
+                if(g.get(i).size() == t.get(j).size() && !check[j]) {
+                    if(isCompare(g.get(i), t.get(j))) {
+                        check[j] = true;
+                        result += t.get(j).size();
+                        break;
+                    }
                 }
-                else game_board[i][j] = 1;
             }
         }
         
-        boolean[][] visited_t = new boolean[len][len];
-        boolean[][] visited_g = new boolean[len][len];
-        
-        for(int i=0; i<len; i++){
-            for(int j=0; j<len; j++){
-                if(table[i][j]==1 && !visited_t[i][j])
-                    bfs(i, j, table, visited_t, t);
-                    
-                if(game_board[i][j]==1 && !visited_g[i][j])
-                    bfs(i, j, game_board, visited_g, g);
-            }
-        }
-
-        answer = compareBlock(t, g, answer);
-        
-        return answer;
+        return result;
     }
     
-    public int compareBlock(List<List<Point>> table, List<List<Point>> board, int answer){
-        int table_size = table.size();
-        int board_size = board.size();
-        
-        boolean[] visited = new boolean[board_size];
-        
-        for(int i=0; i<table_size; i++){
-            for(int j=0; j<board_size; j++){
-                if(visited[j] || table.get(i).size()!=board.get(j).size())
-                    continue;
-                if(isRotate(table.get(i), board.get(j))){
-                    visited[j] = true;
-                    answer += board.get(j).size();
-                    break;   
-                }
-               
-            }
-        }
-        
-        return answer;
-    }
-    
-    public boolean isRotate(List<Point> table, List<Point> board){
-        Collections.sort(board);
-        
-        for(int i=0; i<4; i++){
-            Collections.sort(table);
+    public boolean isCompare(List<int[]> a, List<int[]> b) {
+        boolean isSame = false;
+        List<int[]> rotated = b;
+        for(int k = 0; k < 4; k++) {
+            rotated = rotate(rotated);
+            boolean flag = true;
+            for(int i = 0; i < a.size(); i++) {
+                int[] point1 = a.get(i);
+                int[] point2 = rotated.get(i);
             
-            int curr_x = table.get(0).x;
-            int curr_y = table.get(0).y;
-            
-            for(int j=0; j<table.size(); j++){
-                table.get(j).x -= curr_x;
-                table.get(j).y -= curr_y;
-            }
-            
-            boolean check = true;
-
-            for(int j=0; j<board.size(); j++){
-                if(board.get(j).x != table.get(j).x || board.get(j).y != table.get(j).y){
-                    check = false;
+                if(point1[0] != point2[0] || point1[1] != point2[1]) {
+                    flag = false;
                     break;
                 }
             }
-            
-            if(check){
-                return true;
+            if(flag) {
+                isSame = true;
+                break;
             }
-            else{
-                for(int j=0; j<table.size(); j++){
-                    int temp = table.get(j).x;
-                    table.get(j).x = table.get(j).y;
-                    table.get(j).y = -temp;
-                }
-            }         
         }
         
-        return false;
+        return isSame;
     }
     
-    public void bfs(int x, int y, int[][] board, boolean[][] visited, List<List<Point>> list){        
-        visited[x][y] = true;
-        
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x, y));
-        
-        List<Point> sub_list = new ArrayList<>();
-        sub_list.add(new Point(0, 0));
-        
-        while(!q.isEmpty()){
-            Point p = q.poll();
+    public List<int[]> rotate (List<int[]> a) {
+        List<int[]> newOne = new ArrayList<>();
+        for(int i = 0; i < a.size(); i++) {
+            int[] point = a.get(i);
+            newOne.add(new int[] {point[1], -point[0]});
             
-            for(int i=0; i<4; i++){
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
+        }
+        
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < newOne.size(); i++) {
+            minX = Math.min(newOne.get(i)[0], minX);
+            minY = Math.min(newOne.get(i)[1], minY);
+        }
+        
+        for(int i = 0; i < newOne.size(); i++) {
+            newOne.get(i)[0] -= minX;
+            newOne.get(i)[1] -= minY;
+        }
+        
+        Collections.sort(newOne, (o1, o2) -> {
+            if(o1[0] == o2[0]) return Integer.compare(o1[1],o2[1]);
+            
+            return Integer.compare(o1[0],o2[0]);
+        });
+        return newOne;
+    }
+    
+    public List<int[]> bfs(int num, int[][] map, int tx, int ty) {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[] {tx, ty});
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {tx, ty});
+        map[tx][ty] = -1;
+        
+        while(!q.isEmpty()) {
+            int[] point = q.poll();
+            int x = point[0];
+            int y = point[1];
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(nx<0 || ny<0 || nx>=board.length || ny>=board.length) continue;
-                
-                if(!visited[nx][ny] && board[nx][ny]==1){
-                    visited[nx][ny] = true;
-                    q.add(new Point(nx, ny));
-                    sub_list.add(new Point(nx-x, ny-y));
+                if(nx >= 0 && nx < map.length && ny >= 0 && ny < map.length && map[nx][ny] == num) {
+                    list.add(new int[]{nx, ny});
+                    q.add(new int[]{nx, ny});
+                    map[nx][ny] = -1;
                 }
             }
         }
         
-        list.add(sub_list);
-    }
-    
-    static class Point implements Comparable<Point>{
-        int x, y;
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < list.size(); i++) {
+            minX = Math.min(list.get(i)[0], minX);
+            minY = Math.min(list.get(i)[1], minY);
         }
         
-        public int compareTo(Point o){
-            int res = Integer.compare(this.x, o.x);
-            if(res==0){
-                res = Integer.compare(this.y, o.y);
-            }
-            return res;
+        for(int i = 0; i < list.size(); i++) {
+            list.get(i)[0] -= minX;
+            list.get(i)[1] -= minY;
         }
+        
+        Collections.sort(list, (o1, o2) -> {
+            if(o1[0] == o2[0]) return Integer.compare(o1[1],o2[1]);
+            
+            return Integer.compare(o1[0],o2[0]);
+        });
+        return list;
     }
 }
